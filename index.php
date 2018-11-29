3ce421cf70db847462f4a3b0f5f4c808a913b766
RubOS97 <31545208+RubOS97@users.noreply.github.com>
2018-11-29 13:34:41 +0100

    creación del partial nav

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-require 'utils/utils.php';
-require 'views/index.view.php';
+    require 'entity/ImagenGaleria.php';
+    $imagenes=[];
+    for ($i=1; $i<=12; $i++){
+        $im=new ImagenGaleria($i.'.jpg', 'descripcion imagen '.$i, $i, $i, $i);
+        //echo $im->getNombre();
+        $imagenes[$i] = $im;
+        //echo $imagenes[$i]->getNombre();
+    }
+    require 'utils/utils.php';
+    require 'views/index.view.php';
